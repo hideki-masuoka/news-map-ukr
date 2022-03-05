0146c1cd5678e5f4104bcec10fcb9a28697f2322
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-05 12:15:03 +0900

    add tweet component

---
+++ src/lib/embedTweet.svelte
@@ -0,0 +1,40 @@
+<script>
+import {onMount,afterUpdate} from 'svelte';
+
+export let area = {};
+
+export let tweetdata = [];
+
+const getAreaName = (id) => {
+    return area[id].name ?? '';
+};
+
+onMount( ()=>{
+    tweetdata.forEach((item)=>{
+       twttr.widgets.createTweet(
+            item.id,
+            document.getElementById('twt' + item.id),
+            {
+                width: 300,
+                align: 'center',
+            }
+       );
+    });
+});
+
+
+afterUpdate(()=>{
+    //twttr.widgets.load();
+});
+
+</script>
+
+
+
+<section class="embedTweets">
+{#each tweetdata as item}
+    <div id="twt{item.id}"><strong>{getAreaName(item.area)}</strong></div>
+{/each}
+</section>
+<style >
+</style>
