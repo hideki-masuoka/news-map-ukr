f43f7ce1ca945210b635e8330238634770c825d8
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-05 10:49:57 +0900

    Package init

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import WindiCSS from 'vite-plugin-windicss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		  adapter: adapter({
+          pages: 'build',
+          assets: 'build',
+          fallback: null,
+          precompress: false
+      }),
+      vite: {
+          plugins: [
+              WindiCSS(),
+          ]
+      }
 	}
 };
 
