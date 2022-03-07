c55e45df1ce16c68f33f8b30391a3b30a983da6f
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-07 10:11:47 +0900

    add link liveuamap

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -4,6 +4,12 @@
 		これはウクライナのニュースを読むときに、どの地域のことを指しているのかが分かるようにしたものです。
 	</p>
 	<p>ニュース・ソースはTwitter、地図はWikipediaをそれぞれ参照し、手動で追加しています。</p>
+	<hr>
+	<p class="text-sm text-right">
+	※さらに詳細を知るには、
+	<a href="https://liveuamap.com" target="_blank" rel="noopener">Live Universal Awareness Map</a>
+	をチェックしてみてください。
+	</p>
 </div>
 
 <style>
@@ -12,6 +18,16 @@
 
 		h2 {
 			@apply text-xl font-bold;
+		}
+		hr {
+		  @apply my-2;
+		}
+		a {
+		  color: #0000ff;
+		  @apply font-bold;
+		  &:hover {
+		    opacity: 0.75;
+		    }
 		}
 	}
 </style>
