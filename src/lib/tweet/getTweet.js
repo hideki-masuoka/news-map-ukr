4c9bc5f5f499478ee5a48e31d731a7c968dff71d
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-11 07:00:30 +0900

    add ksort

--- src/lib/tweet/getTweet.js
+++ src/lib/tweet/getTweet.js
@@ -1,16 +1,24 @@
 import alltweet from '$lib/json/tweet.json';
 //import details from '$lib/json/oembed.json';
 
+const ksort = (a, b) => {
+	if (a > b) return -1;
+	if (a < b) return 1;
+	return 0;
+};
+
 export const getFromDate = (date, data) => {
 	let arr = [];
-	Object.entries(alltweet).forEach(([k, obj]) => {
-		if (date === obj.date) {
-			arr.push({
-				id: k,
-				area: obj.area,
-				embed: data[k] ?? {}
-			});
-		}
-	});
+	Object.entries(alltweet)
+		.sort(ksort)
+		.forEach(([k, obj]) => {
+			if (date === obj.date) {
+				arr.push({
+					id: k,
+					area: obj.area,
+					embed: data[k] ?? {}
+				});
+			}
+		});
 	return arr;
 };
