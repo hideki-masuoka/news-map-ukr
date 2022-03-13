fa70c3eedbfb6e1ee92a21ecdce45e4b85dabf58
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-13 16:28:33 +0900

    add tweets

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -3,5 +3,5 @@ import { readable } from 'svelte/store';
 export const SITE_META = readable({
 	title: 'ニュースマップ：ウクライナ',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
-	lastupdate: '2022-03-13 07:59'
+	lastupdate: '2022-03-13 16:25'
 });
