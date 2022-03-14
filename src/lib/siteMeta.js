912a7caa0bb9fa1086ea177e66abb4a6c515a411
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-14 21:09:56 +0900

    add tweets
    add map area
    fix wikipedia link

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -3,5 +3,5 @@ import { readable } from 'svelte/store';
 export const SITE_META = readable({
 	title: 'ニュースマップ：ウクライナ',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
-	lastupdate: '2022-03-14 07:25'
+	lastupdate: '2022-03-14 20:30'
 });
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
 	import DaysPage from '$lib/DaysPage.svelte';
 	import { getFromDate } from '$lib/tweet/getTweet.js';
-	import jsonData from '$lib/json/daily/oembed20220313.json';
+	import jsonData from '$lib/json/daily/oembed20220314.json';
 
 	const pageTitle = 'News';
-	const tweetdata = getFromDate('20220313', jsonData);
+	const tweetdata = getFromDate('20220314', jsonData);
 </script>
 
 <DaysPage {tweetdata} {pageTitle} embed={false} />
