bfebe843b0edb642f17893f0f31e5294afd86e6a
hideki-masuoka <hansendo.d3s@gmail.com>
2022-03-25 14:25:55 +0900

    add tweets

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -2,11 +2,10 @@ import { readable } from 'svelte/store';
 
 export const SITE_META = readable({
 	title: 'ニュースマップ：ウクライナ',
-	description1:
-		'これはウクライナのニュースを読むときに、どの地域のことを指しているのかが分かるようにしたものです。',
+	description1: 'これはウクライナに関する報道ツイートを地図上の地域にマッピングしたものです。',
 	description2:
-		'ニュース・ソースはTwitter、地図はWikipediaをそれぞれ参照し、手動で追加しています。',
+		'地理情報はWikipedia、ニュース・ソースはTwitterをそれぞれ参照し、手動で追加しています。',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
 	hosting: 'https://news-map-ukr.web.app',
-	lastupdate: '2022-03-24 21:42'
+	lastupdate: '2022-03-25 12:24'
 });
--- src/routes/days/d20220324.svelte
+++ src/routes/days/d20220324.svelte
@@ -1,10 +1,10 @@
 <script>
 	import DaysPage from '$lib/DaysPage.svelte';
 	import { getFromDate } from '$lib/tweet/getTweet.js';
-	import jsonData from '$lib/json/daily/oembed20220325.json';
+	import jsonData from '$lib/json/daily/oembed20220324.json';
 
-	const pageTitle = 'News';
-	const tweetdata = getFromDate('20220325', jsonData);
+	const pageTitle = '2022年3月24日';
+	const tweetdata = getFromDate('20220324', jsonData);
 </script>
 
 <DaysPage {tweetdata} {pageTitle} embed={false} />
