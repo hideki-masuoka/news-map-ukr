fb503985e9825f0a931374e5a3eef7be2d908cc2
hideki-masuoka <hansendo.d3s@gmail.com>
2022-04-04 12:31:18 +0900

    add tweets

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -7,5 +7,5 @@ export const SITE_META = readable({
 		'地理情報はWikipedia、ニュース・ソースはTwitterをそれぞれ参照し、手動で追加しています。',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
 	hosting: 'https://news-map-ukr.web.app',
-	lastupdate: '2022-04-03 22:02'
+	lastupdate: '2022-04-04 12:04'
 });
--- src/routes/days/d20220403.svelte
+++ src/routes/days/d20220403.svelte
@@ -1,10 +1,10 @@
 <script>
 	import DaysPage from '$lib/DaysPage.svelte';
 	import { getFromDate } from '$lib/tweet/getTweet.js';
-	import jsonData from '$lib/json/daily/oembed20220404.json';
+	import jsonData from '$lib/json/daily/oembed20220403.json';
 
-	const pageTitle = 'News';
-	const tweetdata = getFromDate('20220404', jsonData);
+	const pageTitle = '2022年4月3日';
+	const tweetdata = getFromDate('20220403', jsonData);
 </script>
 
 <DaysPage {tweetdata} {pageTitle} embed={false} />
