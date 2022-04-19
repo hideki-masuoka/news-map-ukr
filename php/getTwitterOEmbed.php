eedbce0d48ec40fc312d5a90a335d7ee1e4a261f
hideki-masuoka <hansendo.d3s@gmail.com>
2022-04-19 22:50:51 +0900

    add option stream_context

--- php/getTwitterOEmbed.php
+++ php/getTwitterOEmbed.php
@@ -15,6 +15,13 @@ $tweetdata = json_decode($targetjson, true);
 
 $embedparams = "https://publish.twitter.com/oembed?maxwidth=280&hide_thread=t&omit_script=t&align=center&dnt=true&url=https%3A%2F%2Ftwitter.com%2Fname%2Fstatus%2F";
 
+$stream_context = stream_context_create(
+    ['ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false
+    ]]
+);
+
 $retdata = [];
 
 foreach($tweetdata as $k => $item) {
@@ -23,8 +30,10 @@ foreach($tweetdata as $k => $item) {
                  continue;
 }
   $tweetid = (string)$k;
-  $data = file_get_contents($embedparams . $tweetid);
+  $data = file_get_contents($embedparams . $tweetid, false, $stream_context);
   $retdata[$date][$k] = json_decode($data, true);
+
+  usleep(300000);
 }
 
 if (empty($retdata)) {
