56699c15228cd2ac9139c2b13401f8fa1071b95d
hideki-masuoka <hansendo.d3s@gmail.com>
2022-05-23 13:47:16 +0900

    add tweets

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
 	import DaysPage from '$lib/DaysPage.svelte';
 	import { getFromDate } from '$lib/tweet/getTweet.js';
-	import jsonData from '$lib/json/daily/oembed20220522.json';
+	import jsonData from '$lib/json/daily/oembed20220523.json';
 
 	const pageTitle = 'News';
-	const tweetdata = getFromDate('20220522', jsonData);
+	const tweetdata = getFromDate('20220523', jsonData);
 </script>
 
 <DaysPage {pageTitle} {tweetdata} embed={false} />
