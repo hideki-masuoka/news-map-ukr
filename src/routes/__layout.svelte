6c2c3d3ce66995ee52f2480ad67e51005f30d2af
hideki-masuoka <hansendo.d3s@gmail.com>
2022-07-07 16:54:22 +0900

    add GoatCounter

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,24 +3,32 @@
 	import 'virtual:windi.css';
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
+
+	const USE_GTM = false;
 </script>
 
 <svelte:head>
 	{#if !dev}
-		<!-- Google Tag Manager -->
-		<script>
-			(function (w, d, s, l, i) {
-				w[l] = w[l] || [];
-				w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-				var f = d.getElementsByTagName(s)[0],
-					j = d.createElement(s),
-					dl = l != 'dataLayer' ? '&l=' + l : '';
-				j.async = true;
-				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-				f.parentNode.insertBefore(j, f);
-			})(window, document, 'script', 'dataLayer', 'GTM-NLRVFJK');
-		</script>
-		<!-- End Google Tag Manager -->
+		<script
+			data-goatcounter="https://hansendo.goatcounter.com/count"
+			async
+			src="//gc.zgo.at/count.js"></script>
+		{#if USE_GTM}
+			<!-- Google Tag Manager -->
+			<script>
+				(function (w, d, s, l, i) {
+					w[l] = w[l] || [];
+					w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+					var f = d.getElementsByTagName(s)[0],
+						j = d.createElement(s),
+						dl = l != 'dataLayer' ? '&l=' + l : '';
+					j.async = true;
+					j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+					f.parentNode.insertBefore(j, f);
+				})(window, document, 'script', 'dataLayer', 'GTM-NLRVFJK');
+			</script>
+			<!-- End Google Tag Manager -->
+		{/if}
 	{/if}
 </svelte:head>
 
