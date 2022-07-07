993b646a70ab55bc879165c2201ae1201c5c36e6
hideki-masuoka <hansendo.d3s@gmail.com>
2022-07-07 16:17:00 +0900

    add Google Tag Manager

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,28 @@
 <script>
+	import { dev } from '$app/env';
 	import 'virtual:windi.css';
 	import Header from '$lib/Header.svelte';
 	import Footer from '$lib/Footer.svelte';
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<!-- Google Tag Manager -->
+		<script>
+			(function (w, d, s, l, i) {
+				w[l] = w[l] || [];
+				w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+				var f = d.getElementsByTagName(s)[0],
+					j = d.createElement(s),
+					dl = l != 'dataLayer' ? '&l=' + l : '';
+				j.async = true;
+				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+				f.parentNode.insertBefore(j, f);
+			})(window, document, 'script', 'dataLayer', 'GTM-NLRVFJK');
+		</script>
+		<!-- End Google Tag Manager -->
+	{/if}
+</svelte:head>
 
 <Header />
 
