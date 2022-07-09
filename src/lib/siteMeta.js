6387ddfbf06b35a8a19a750f5f9a7a0678982205
hideki-masuoka <hansendo.d3s@gmail.com>
2022-07-09 17:58:22 +0900

    add tweets

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -8,7 +8,7 @@ export const SITE_META = readable({
 		'地理情報はWikipedia、ニュース・ソースはTwitterをそれぞれ参照し、手動で追加しています。',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
 	hosting: 'https://news-map-ukr.web.app',
-	lastupdate: '2022-07-09 11:01'
+	lastupdate: '2022-07-09 17:44'
 });
 
 export const GOAT_COUNTER = dev ? '' : import.meta.env.VITE_GOAT_COUNTER;
