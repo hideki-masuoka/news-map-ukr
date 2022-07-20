f1a6a6a0c34b8847c811a043cf8dcc103291bd04
hideki-masuoka <hansendo.d3s@gmail.com>
2022-07-20 23:51:27 +0900

    add tweets

---
+++ src/routes/days/d20220719.svelte
@@ -0,0 +1,10 @@
+<script>
+	import DaysPage from '$lib/DaysPage.svelte';
+	import { getFromDate } from '$lib/tweet/getTweet.js';
+	import jsonData from '$lib/json/daily/oembed20220719.json';
+
+	const pageTitle = '2022年7月19日';
+	const tweetdata = getFromDate('20220719', jsonData);
+</script>
+
+<DaysPage {tweetdata} {pageTitle} embed={false} />
