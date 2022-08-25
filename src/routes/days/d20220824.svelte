de5b31cae0b7ba2acde58b765ae256f50305214e
hansendo <hansendo@localhost.localdomain>
2022-08-25 21:22:04 +0900

    add tweets

---
+++ src/routes/days/d20220824.svelte
@@ -0,0 +1,10 @@
+<script>
+	import DaysPage from '$lib/DaysPage.svelte';
+	import { getFromDate } from '$lib/tweet/getTweet.js';
+	import jsonData from '$lib/json/daily/oembed20220824.json';
+
+	const pageTitle = '2022年8月24日';
+	const tweetdata = getFromDate('20220824', jsonData);
+</script>
+
+<DaysPage {tweetdata} {pageTitle} embed={false} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
 	import DaysPage from '$lib/DaysPage.svelte';
 	import { getFromDate } from '$lib/tweet/getTweet.js';
-	import jsonData from '$lib/json/daily/oembed20220824.json';
+	import jsonData from '$lib/json/daily/oembed20220825.json';
 
 	const pageTitle = 'News';
-	const tweetdata = getFromDate('20220824', jsonData);
+	const tweetdata = getFromDate('20220825', jsonData);
 </script>
 
 <DaysPage {pageTitle} {tweetdata} embed={false} />
