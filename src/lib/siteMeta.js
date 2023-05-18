9a6d979b4d9fe15ed6c43baa6a77cfb46ebbce7e
hansendo <hansendo@localhost.localdomain>
2023-05-19 00:03:25 +0900

    add data

--- src/lib/siteMeta.js
+++ src/lib/siteMeta.js
@@ -8,7 +8,7 @@ export const SITE_META = readable({
 		'地理情報はWikipedia、ニュース・ソースはTwitterをそれぞれ参照し、手動で追加しています。',
 	repo: 'https://github.com/hideki-masuoka/news-map-ukr',
 	hosting: 'https://news-map-ukr.web.app',
-	lastupdate: '2023-05-15 17:20'
+	lastupdate: '2023-05-18 23:55'
 });
 
 export const GOAT_COUNTER = dev ? '' : import.meta.env.VITE_GOAT_COUNTER;
@@ -32,8 +32,8 @@ export const MonthlyLinkList = {
 		{ m: '2', last: '28' },
 		{ m: '3', last: '31'},
 		{ m: '4', last: '30'},
-		{ m: '5', last: '13'}
+		{ m: '5', last: '16'}
 	]
 };
 
-export const CurrentDay = '20230514';
+export const CurrentDay = '20230517';
